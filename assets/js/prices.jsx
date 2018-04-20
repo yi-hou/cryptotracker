6d2d1afd02ecc14ec429ec81a2e503326a935060
Henry Zhou <henryzhou95@gmail.com>
2018-04-20 19:31:54 -0400

    navbar

--- assets/js/prices.jsx
+++ assets/js/prices.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Button } from 'reactstrap';
+import Select from 'react-select';
 
 
 
@@ -19,6 +20,7 @@ class Demo extends React.Component {
         this.state = {
             coinSymbols: ["BTC", "ETH", "LTC", "BCH"],
             coinName: ["Bitcoin", "Ethereum", "Litecoin", "Bitcoin Cash"],
+            coinPRICEPREV: [0, 0, 0, 0],
             coinPRICE: [0, 0, 0, 0],
             coinSUPPLY: [0, 0, 0, 0],
             coinTOTALVOLUME24H: [0, 0, 0, 0],
@@ -26,8 +28,13 @@ class Demo extends React.Component {
             coinHIGH24HOUR: [0, 0, 0, 0],
             coinLOW24HOUR: [0, 0, 0, 0],
             coinToggle: [true, true, true, true],
+            coinExchange: ["USD", "BTC", "EUR"],
+            selectedExchange: "USD",
+            timerId: 0
         };
-        this.getAPIDetails()
+      
+        this.getAPIDetailsUSD("USD")
+
     }
 
     handleToggle(coinToggleIndex) {
@@ -37,6 +44,7 @@ class Demo extends React.Component {
     }
 
     render() {
+        this.changeColor();
         return (
             <div className="container">
                 <div className="checkbox-container">
@@ -54,8 +62,19 @@ class Demo extends React.Component {
                                     {symbol}
                                 </button>
                     })}
+                    
+                    
+                    <select name="input" label="Select Currency" defaultValue="USD">
+                        <option value="USD" onClick = {() => this.currencyChanged("USD")}> USD</option>
+                        <option value="EUR" onClick = {() => this.currencyChanged("EUR")}> EUR </option>
+                        <option value="BTC" onClick = {() => this.currencyChanged("BTC")}> BTC </option>
+                        </select>
+                       
+                   
+                   
                 </div>
-                <table className="table table-dark">
+                <div id="divPrice">
+                <table id="tPrice" className="table table-dark">
                     <thead>
                         <tr>
                         <th scope="col">#</th>
@@ -73,26 +92,28 @@ class Demo extends React.Component {
                         {this.state.coinToggle.map((toggleValue, index) => {
                             if (toggleValue){
                                 return <tr>
-                                        <th scope="row">{index}</th>
-                                        <td>{this.state.coinSymbols[index]}</td>
-                                        <td>{this.state.coinName[index]}</td>
-                                        <td>{this.state.coinPRICE[index]}</td>
-                                        <td>{this.state.coinMKTCAP[index]}</td>
-                                        <td>{this.state.coinSUPPLY[index]}</td>
-                                        <td>{this.state.coinTOTALVOLUME24H[index]}</td>
-                                        <td>{this.state.coinHIGH24HOUR[index]}</td>
-                                        <td>{this.state.coinLOW24HOUR[index]}</td>
+                                        <th scope="row">{index + 1}</th>
+                                        <td>
+                                        <label id="labelSymbol">{this.state.coinSymbols[index]}</label></td>
+                                        <td><label id="labelName">{this.state.coinName[index]}</label></td>
+                                        <td><label onLoad='changeColor()' id="labelPrice">{this.state.coinPRICE[index]}</label></td>
+                                        <td><label id="labelMktcap">{this.state.coinMKTCAP[index]}</label></td>
+                                        <td><label id="labelSupply">{this.state.coinSUPPLY[index]}</label></td>
+                                        <td><label id="labelVol">{this.state.coinTOTALVOLUME24H[index]}</label></td>
+                                        <td><label id="labelHigh">{this.state.coinHIGH24HOUR[index]}</label></td>
+                                        <td><label id="labelLow">{this.state.coinLOW24HOUR[index]}</label></td>
                                         </tr>
                             }
                         })}
                     </tbody>
                 </table>
+                </div>
                 <div>
-                    <button 
-                    type="button" 
+                    <button id="btnFetchData"
+                    type="button" visible="false"
                     className="btn btn-primary btn-coin-toggle" 
                     data-toggle="button" 
-                    onClick = {() => this.getAPIDetails()}>
+                    onClick = {() => this.getAPIDetailsUSD()}>
                     Fetch Data
                     </button>
                 </div>
@@ -100,11 +121,128 @@ class Demo extends React.Component {
         )
     }
 
-    getAPIDetails() {
-        this.channel.push("fetch_prices", { coinnames: this.state.coinSymbols, state: this.state })
+   
+   currencyChanged(currency)
+   {
+    clearInterval(this.state.timerId);
+    this.setState({selectedExchange: currency})
+    if(currency == "USD")
+    {
+        getAPIDetailsUSD(currency) 
+    }
+    else if(currency == "EUR")
+    {
+        getAPIDetailsEUR(currency) 
+    }
+    else if(currency == "BTC")
+    {
+        getAPIDetailsBTC(currency) 
+    }
+}
+
+
+ 
+
+    getAPIDetailsUSD() {
+       
+      
+        this.channel.push("fetch_prices_USD", { coinnames: this.state.coinSymbols, state: this.state})
             .receive("ok", this.gotView.bind(this))
+            console.log("Real time data fetched")
+       
+
+     var timer =  setInterval(function() {
+            console.log("tick")
+        document.getElementById("btnFetchData").click();
+    }, 4000)
+
+    this.setState({timerId: timer})
+
     }
 
+    getAPIDetailsEUR() {
+      
+       
+         this.channel.push("fetch_prices_EUR", { coinnames: this.state.coinSymbols, state: this.state})
+             .receive("ok", this.gotView.bind(this))
+             console.log("Real time data fetched")
+        
+ 
+      var timer =  setInterval(function() {
+             
+         document.getElementById("btnFetchData").click();
+     }, 4000)
+ 
+     this.setState({timerId: timer})
+ 
+     }
+   
+     
+    getAPIDetailsBTC() {
+       
+       
+         this.channel.push("fetch_prices_BTC", { coinnames: this.state.coinSymbols, state: this.state})
+             .receive("ok", this.gotView.bind(this))
+             console.log("Real time data fetched")
+        
+ 
+      var timer =  setInterval(function() {
+             
+         document.getElementById("btnFetchData").click();
+     }, 4000)
+ 
+     this.setState({timerId: timer})
+ 
+     }
+    
+   
+    changeColor() {
+        
+        var cp = [];
+        cp = this.state.coinPRICE
+        var cpp = [];
+        cpp = this.state.coinPRICEPREV
+        var rows = document.getElementsByTagName("tr")
+       
+        for (var i = 1; i < rows.length; i++) {
+            var row = rows[i];
+            var cells = row.getElementsByTagName('td');
+            var  cell = cells[2]
+            
+            if(cp[i-1] > cpp[i-1] || cpp[i-1] == 0)
+            {
+                console.log(cp[i-1])
+                console.log(cpp[i-1])
+                cell.style.color = "green"
+                row.style.backgroundColor = "green"
+                row.style.backgroundColor = "black"
+
+            }
+            else if (cp[i-1] < cpp[i-1])
+            {
+                console.log(cp[i-1])
+                console.log(cpp[i-1])
+
+                cell.style.color = "red"
+                row.style.backgroundColor = "red"
+                row.style.backgroundColor = "black"
+            }
+         
+          
+           
+          
+}
+
+
+         
+    }
+
+ 
+
+
+
+   
+    
     gotView(pricedata) {
         for (var symbols in pricedata) {
             var coinStateIndex = this.state.coinSymbols.indexOf(symbols)
@@ -112,8 +250,15 @@ class Demo extends React.Component {
                 var coinData = (pricedata[symbols][currency])
                 for(var key in coinData) {
                     if(key == "PRICE") {
+                       
+                     
+                        this.state.coinPRICEPREV[coinStateIndex] = this.state.coinPRICE[coinStateIndex];
+
+                        this.setState({coinPRICEPREV: this.state.coinPRICEPREV});
                         this.state.coinPRICE[coinStateIndex] = coinData[key];
                         this.setState({coinPRICE: this.state.coinPRICE});
+                       
+                                                
                     }
                     else if(key == "SUPPLY") {
                         this.state.coinSUPPLY[coinStateIndex] = coinData[key];
@@ -140,4 +285,3 @@ class Demo extends React.Component {
         }        
     }
 }
-
